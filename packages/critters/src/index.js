2e31d6a8afc8531467a5f0601d0e6643ec0458f9
Jason Miller <developit@users.noreply.github.com>
2022-01-05 10:29:56 -0500

    update jsdoc source (readme is autogenerated)

--- packages/critters/src/index.js
+++ packages/critters/src/index.js
@@ -39,6 +39,7 @@ import { createLogger } from './util';
  * - **"swap-high":** Use `<link rel="alternate stylesheet preload">` and swap to `rel="stylesheet"` once loaded ([details](http://filamentgroup.github.io/loadCSS/test/new-high.html)). <kbd>JS</kbd>
  * - **"js":** Inject an asynchronous CSS loader similar to [LoadCSS](https://github.com/filamentgroup/loadCSS) and use it to load stylesheets. <kbd>JS</kbd>
  * - **"js-lazy":** Like `"js"`, but the stylesheet is disabled until fully loaded.
+ * - **false:** Disables adding preload tags.
  * @typedef {(default|'body'|'media'|'swap'|'swap-high'|'js'|'js-lazy')} PreloadStrategy
  * @public
  */
